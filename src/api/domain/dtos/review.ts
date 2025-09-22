5a8410c93fa4274fb3c8cfcd80548640f6395633
udara88 <udarawickramasinghe88@gmail.com>
2025-09-22 16:51:47 +0530

    Add review validation

--- src/api/domain/dtos/review.ts
+++ src/api/domain/dtos/review.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 const CreateReviewDto = z.object({
-  rating: z.string().min(1, "Rate is required"),
-  comment: z.string().min(0, "Comment is required"),
+  rating: z.number().min(0, "Rate is required"),
+  comment: z.string().min(1, "Comment is required"),
   hotelId: z.string().min(1, "Hotel ID is required"),
 });
 
